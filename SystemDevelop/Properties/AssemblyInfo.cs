ba8e9274aaeb028b97da95d860933abb3dd81bd2
Kai <aria7716@gmail.com>
2019-10-24 14:14:35 +0900

    sln作成

--- SystemDevelop/Properties/AssemblyInfo.cs
+++ SystemDevelop/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("dd5fbecd-09f2-44f8-a909-8adb7e7ece67")]
+[assembly: Guid("a5244645-d197-40e4-9a6d-b20d2d3032f4")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
