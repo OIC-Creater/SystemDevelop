f9c65ec45ac7c70078cb20c0ec06e4e6f8119d77
Kai <aria7716@gmail.com>
2019-11-01 14:59:49 +0900

    hash化できるように

---
+++ SystemDevelop.Test/PasswordHashTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemDevelop.Test
+{
+    [TestFixture]
+    class PasswordHashTests
+    {
+        [Test]
+        public void HashTest()
+        {
+            //Assert.True();
+        }
+    }
+}
---
+++ SystemDevelop/Model/PaasswordHash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemDevelop.Model
+{
+    /// <summary>
+    /// passwordをハッシュ化するためのクラス
+    /// </summary>
+    class PaasswordHash
+    {
+        //hash用のprovider
+        static readonly SHA256CryptoServiceProvider hashProvider = new SHA256CryptoServiceProvider();
+
+        //passwordをハッシュ化する
+        public string Hash(string password)
+        {
+            return string.Join("", hashProvider.ComputeHash(Encoding.UTF8.GetBytes(password)).Select(x => $"{x:x2}"));
+        }
+    }
+}
