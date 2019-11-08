bc2c76bc21d286b364be29b6c0a3b56745c8eec4
Neiki <tutiya_kota@icloud.com>
2019-11-08 16:28:16 +0900

    ログイン機能実装

---
+++ SystemDevelop/Model/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemDevelop.Model.DB;
+
+namespace SystemDevelop.Model
+{
+    class Login
+    {
+        private DbCreate dbCreate = new DbCreate();
+        public Login()
+        {
+            dbCreate.ConnectDb();
+        }
+    }
+}
