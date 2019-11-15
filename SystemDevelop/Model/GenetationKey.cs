ea2908cf0af3c5bba08bb17d0769676cd5d9755a
Neiki <tutiya_kota@icloud.com>
2019-11-15 18:00:33 +0900

    鍵の生成と保存実装

---
+++ SystemDevelop/Model/GenetationKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace SystemDevelop.Model
+{
+    class GenetationKey
+    {
+        private void CreateKey()
+        {
+            //新規鍵の設定
+            int size = 1024;
+            //新規鍵の生成
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider(size);
+            //公開鍵をXMLで取得
+            string publickkey = csp.ToXmlString(false);
+            //秘密鍵をXMLで取得
+            string privateKey = csp.ToXmlString(true);
+        }
+    }
+}
