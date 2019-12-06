9e4dd128764c2cd62017ecfacf7e1e4a2f83fa7a
kotone <ranmaru0329@i.softbank.jp>
2019-12-06 17:57:10 +0900

    ネーム変更

--- SystemDevelop/DataModels/ReciveOrderDetail.cs
+++ SystemDevelop/DataModels/ReciveOrderDetail.cs
@@ -6,10 +6,9 @@ namespace SystemDevelop.DataModels
     {
         
         public string ReciveOrderDetailID { get; set; }
-
         public string ReciveOrderID { get; set; }
         public string ProductID { get; set; }
-        public int quantity { get; set; }
+        public int Quantity { get; set; }
         
         public void Update() { }
         public void Get() { }
