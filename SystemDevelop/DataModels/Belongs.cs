f6eed99757afc372413af7fe310d1597760470db
Kai <aria7716@gmail.com>
2019-12-10 15:20:26 +0900

    hoge

--- SystemDevelop/DataModels/Belongs.cs
+++ SystemDevelop/DataModels/Belongs.cs
@@ -1,11 +1,13 @@
-﻿using SystemDevelop.Interface;
+﻿using System.Data.OleDb;
+using SystemDevelop.Interface;
 
 namespace SystemDevelop.DataModels
 {
     class Belongs : IDatabese
     {
-        public string AffiliationID { get; set; } = "";
-        public string AffiliationName { get; set; } = "";
+        public OleDbConnection OleDb { get; set; }
+        public string AffiliationID { get; set; }
+        public string AffiliationName { get; set; }
 
         public void Update() { }
         public void Get() { }
